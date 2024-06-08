91ce7f77552a36f51c0cd9ced1d08b9e750d979f
Le0v1n <zjkljd@163.com>
2024-06-08 22:48:35 +0800

    Update xml2yolo.sh

--- utils/dataset/detection/xml2yolo.sh
+++ utils/dataset/detection/xml2yolo.sh
@@ -1,1 +1,6 @@
-python utils/dataset/detection/xml2yolo-remaster.py --classes person bicycle car motorcycle airplane bus train truck boat traffic_light fire_hydrant stop_sign parking_meter bench bird cat dog horse sheep cow elephant bear zebra giraffe backpack umbrella handbag tie suitcase frisbee skis snowboard sports_ball kite baseball_bat baseball_glove skateboard surfboard tennis_racket bottle wine_glass cup fork knife spoon bowl banana apple sandwich orange broccoli carrot hot_dog pizza donut cake chair couch potted_plant bed dining_table toilet tv laptop mouse remote keyboard cell_phone microwave oven toaster sink refrigerator book clock vase scissors teddy_bear hair_drier toothbrush
+python utils/dataset/detection/xml2yolo-remaster.py \
+    --image-path Datasets/VOC2012-5000/VOC2012/images \
+    --label-path Datasets/VOC2012-5000/VOC2012/xmls.bak \
+    --target-path Datasets/VOC2012-5000/VOC2012/yolos \
+    --num-threading 32 \
+    --classes "aeroplane" "bicycle" "bird" "boat" "bottle" "bus" "car" "cat" "chair" "cow" "diningtable" "dog" "horse" "motorbike" "person" "pottedplant" "sheep" "sofa" "train" "tvmonitor"
