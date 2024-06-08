42ff7c395798ffc828f9d7d46aeff035d0da0090
Le0v1n <zjkljd@163.com>
2024-06-08 22:48:39 +0800

    Create yolo2xml.sh

---
+++ utils/dataset/detection/yolo2xml.sh
@@ -0,0 +1,6 @@
+python utils/dataset/detection/yolo2xml-remaster.py \
+    --image-path Datasets/VOC2012-5000/VOC2012/images \
+    --label-path Datasets/VOC2012-5000/VOC2012/yolos \
+    --target-path Datasets/VOC2012-5000/VOC2012/xmls \
+    --num-threading 32 \
+    --classes "aeroplane" "bicycle" "bird" "boat" "bottle" "bus" "car" "cat" "chair" "cow" "diningtable" "dog" "horse" "motorbike" "person" "pottedplant" "sheep" "sofa" "train" "tvmonitor"
