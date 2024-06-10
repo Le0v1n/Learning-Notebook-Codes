4e29cba9579d605a9a627ad4b6a1991389519ffa
Le0v1n <zjkljd@163.com>
2024-06-10 23:14:51 +0800

    Update create_empty_labels.py

--- utils/dataset/detection/create_empty_labels.py
+++ utils/dataset/detection/create_empty_labels.py
@@ -120,7 +120,7 @@ if __name__ == "__main__":
     LOGGER.info(dict2table(RECORDER, align='l', replace_keys=TranslationDict))
     
     # 2FA
-    second_confirm(script=FILE)
+    second_confirm(script=FILE, LOGGER=LOGGER)
 
     # 记录
     RECORDER['touch'] = 0
