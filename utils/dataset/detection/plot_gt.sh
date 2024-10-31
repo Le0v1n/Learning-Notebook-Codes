4d584cc3bd8e6fb22847d0ef84717fc1efe5ca4a
Le0v1n <zjkljd@163.com>
2024-10-31 20:10:24 +0800

    Update files

---
+++ utils/dataset/detection/plot_gt.sh
@@ -0,0 +1,8 @@
+python utils/dataset/detection/plot_gt.py \
+    --images idc_cleanliness_detect-all \
+    --labels idc_cleanliness_detect-all \
+    --label_suffix .txt \
+    --class_id 0 1 2 \
+    --class_names carton others footprint \
+    --save_dir idc_cleanliness_detect-all/plots \
+    --save_all
